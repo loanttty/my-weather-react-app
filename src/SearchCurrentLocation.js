42dec2830fe9387fc52f19ad0ef8f937ec895f91
loanttty <loan.tran2802@gmail.com>
2021-04-15 14:50:57 +0800

    Fixed bug in SearchCurrentLocation.js

--- src/SearchCurrentLocation.js
+++ src/SearchCurrentLocation.js
@@ -1,35 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import axios from 'axios';
 
 export default function SearchCurrentLocation(props) {
-	let [location,setLocation] = useState({
-    	lon: null,
-    	lat: null,
-		apiCall: false
-  })
 
 	function clickCurrentLocation (event) {
     event.preventDefault();
     navigator.geolocation.getCurrentPosition((response) => {
-      setLocation({
-        lat: response.coords.latitude,
-        lon: response.coords.longitude,
-		apiCall: true
-      })
+			let lat = response.coords.latitude
+			let lon =  response.coords.longitude
+	  	const key = "9ec2a4429dcdbe4e388875969b764e7e";
+		  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&units=${props.tempUnitIndicator}`;
+		  axios.get(url).then((response) => {
+			  props.updateCurrentWeather(response);
+		  })
 		})
   }
 
-	useEffect(() => {
-		const key = "9ec2a4429dcdbe4e388875969b764e7e";
-		if (location.apiCall) {
-			let url = `https://api.openweathermap.org/data/2.5/weather?lat=${location.lat}&lon=${location.lon}&appid=${key}&units=${props.tempUnitIndicator}`;
-			axios.get(url).then((response) => {
-				props.updateCurrentWeather(response);
-			})
-			setLocation({apiCall: false})
-		}
-	})
-	
 	return (
 		<div className='searchCurrentLocation'>
 			<button className=" btn search-current" onClick={clickCurrentLocation} >
